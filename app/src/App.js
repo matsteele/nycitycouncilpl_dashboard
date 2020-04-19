3bfd7630b517f345bf1b239860ad891eb9c8a46b
matsteele <matsteele@gmail.com>
2020-04-19 18:13:58 -0400

    build first graph, started on map

--- app/src/App.js
+++ app/src/App.js
@@ -1,32 +1,50 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-import csv from 'csvtojson'
 
-import mentions from './data/councilseat_mentions.csv'
+import mentions from './data/councilseat_mentions.json'
+const vals2names = {}
+Object.keys(mentions.mentions).forEach(i=>{
+  vals2names[mentions.mentions[i]] = { name: mentions.name[i], gender:mentions.gender[i]}
+}) 
 
+const colors = {
+  greygreen: '#b6cac0',
+  emphasis: '#7ED2A8',
+  dark:'#2C4A3B',
+  basic: '#87968F'
+}
 
-const App = () =>{
+const App = () => {
+  const width = window.innerWidth
+  const chartHeight = window.innerWidth * .15
+  const values = Object.keys(vals2names).sort()
+  const chartBuffer = width*.1
+
   console.log(mentions)
-
-  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Lear
-        </a>
-      </header>
-    </div>
+      <section>
+        <h2>NYC COUNCIL ISSUES</h2>
+        <h3>NLP Analysis of historic press releases</h3>
+      </section>
+
+      <section>
+        <svg className="si-glyph-button-tv" viewBox={`0 0 ${width} ${chartHeight}`}  >
+          <text x={chartBuffer*.80} y={chartHeight*.75} textAnchor='end' fill={colors.greygreen}>press </text>
+          <text x={chartBuffer*.80} y={chartHeight*.85} textAnchor='end' fill={colors.greygreen}> release </text>
+          <text x={chartBuffer*.80} y={chartHeight*.95} textAnchor='end' fill={colors.greygreen}> mentions</text>
+
+          {values.map(function (val, i) {
+            return (
+              <g key={val}>
+                <line x1={chartBuffer + i * ((width-chartBuffer*2) / values.length)} y1={chartHeight} x2={chartBuffer+i * ((width-chartBuffer*2)/ values.length)}  y2={chartHeight - ( chartHeight* .3 * val)} stroke={colors.greygreen} stroke-width='20' />
+                <text fill={colors.dark} x={chartBuffer + i * ((width-chartBuffer*2) / values.length)} y='50' writingMode="tb" textAnchor='start' direction='ltr' >{vals2names[val].name}</text>
+              </g>
+            )
+          })}
+        </svg>
+      </section>
+    </div >
   )
 }
 
--- app/src/data/toJson.js
+++ app/src/data/toJson.js
@@ -1,18 +1,19 @@
 console.log(require('path').basename('councilseat_mentions.csv'))
 
+console.log(process.cwd())
 
 const csv = require('csvtojson')
-const csvFilePath = 'councilseat_menetions.csv'
+const csvFilePath = './councilseat_mentions.csv'
 
-// csv()
-// .fromFile(csvFilePath)
-// .then((jsonObj)=>{
-//     console.log(jsonObj);
-//     /**
-//      * [
-//      * 	{a:"1", b:"2", c:"3"},
-//      * 	{a:"4", b:"5". c:"6"}
-//      * ]
-//      */ 
-// })
+csv()
+.fromFile(csvFilePath)
+.then((jsonObj)=>{
+    console.log(jsonObj);
+    /**
+     * [
+     * 	{a:"1", b:"2", c:"3"},
+     * 	{a:"4", b:"5". c:"6"}
+     * ]
+     */ 
+})
  
